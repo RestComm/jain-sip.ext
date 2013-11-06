61fccc5b33197ce34dc60b88718bbb5eb7e993e0
Jean Deruelle <jean.deruelle@telestax.com>
2013-11-06 01:11:00 +0100

    Update issue 236
    (cherry picked from telscale commit fd8ec2be4bf9fbe27c997beda885fdaa83632463)
    
    Conflicts:
    	.classpath

--- src/test/java/org/mobicents/ext/javax/sip/dns/DNSServerLocatorTest.java
+++ src/test/java/org/mobicents/ext/javax/sip/dns/DNSServerLocatorTest.java
@@ -1,23 +1,20 @@
 /*
- * JBoss, Home of Professional Open Source
- * Copyright 2011, Red Hat, Inc. and individual contributors
- * by the @authors tag. See the copyright.txt in the distribution for a
- * full listing of individual contributors.
+ * TeleStax, Open Source Cloud Communications
+ * Copyright 2011-2013, Telestax Inc and individual contributors
+ * by the @authors tag.
  *
- * This is free software; you can redistribute it and/or modify it
- * under the terms of the GNU Lesser General Public License as
- * published by the Free Software Foundation; either version 2.1 of
+ * This program is free software: you can redistribute it and/or modify
+ * under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation; either version 3 of
  * the License, or (at your option) any later version.
  *
- * This software is distributed in the hope that it will be useful,
+ * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * Lesser General Public License for more details.
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
  *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this software; if not, write to the Free
- * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
- * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
  */
 
 package org.mobicents.ext.javax.sip.dns;
@@ -105,6 +102,28 @@ public class DNSServerLocatorTest {
 		assertNotNull(hops);
 		assertTrue(hops.size() > 0);
 	}
+	
+	@Test
+	// https://code.google.com/p/sipservlets/issues/detail?id=236
+    public void testResolveBadPhoneNumber() throws ParseException {
+        String transport = ListeningPoint.UDP;
+        int port = 5080;
+        
+        DNSLookupPerformer dnsLookupPerformer = mock(DefaultDNSLookupPerformer.class);
+        dnsServerLocator.setDnsLookupPerformer(dnsLookupPerformer);
+        LinkedList<Hop> mockedHops = new LinkedList<Hop>();
+        mockedHops.add(new HopImpl(LOCALHOST, port, transport));
+        when(dnsLookupPerformer.locateHopsForNonNumericAddressWithPort(host, port, transport.toLowerCase())).thenReturn(mockedHops);
+        
+        sipURI = addressFactory.createSipURI(null,host);
+        sipURI.setParameter("user", "phone");
+        try {
+            SipURI result = dnsServerLocator.getSipURI(sipURI);
+            fail("IllegalArgumentException should be thrown is there is not user part for an uri with a phone parameter");
+        } catch (IllegalArgumentException e) {
+            
+        }        
+    }
 	
 	@Test
 	public void testResolveHostByAandAAAALookup() throws ParseException {
